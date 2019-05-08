85eb330467a123c94d72c7c5deb1cc2a4f13ba8c
RezaRamadhanIrianto <rezaramadhanirianto@gmail.com>
2019-05-08 18:15:35 +0700

    First Commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
-
+ 
 @NgModule({
   imports: [
     CommonModule,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
 
-}
+} 
